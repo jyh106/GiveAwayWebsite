2f8f337832ea50765d492994e880af12e63a8f9b
Hera <jyh106@berkeley.edu>
2019-01-13 18:50:14 -0800

    redesigned Give button

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,15 +3,47 @@ import "./NavBar.css";
 import StyleMenu from './StyleMenu/StyleMenu.js';
 import { connect } from 'react-redux';
 import Actions from '../../Actions/actions.js';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+library.add(faHeart);
 
 class NavBar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            displayHeartOnButton: false,
+        }
+    }
+
+    toggleDisplayHeartOnButton() {
+        this.setState({
+            displayHeartOnButton: !this.state.displayHeartOnButton
+        })
+    }
+
+    newFormButtonLabel() {
+        if (!this.state.displayHeartOnButton) {
+            return "Give"
+        } 
+        return <FontAwesomeIcon icon="heart"></FontAwesomeIcon>
+    }
+    renderNewFormButton() {
+        return (
+            <div className="addNewFormButton"
+                onMouseEnter={()=>this.toggleDisplayHeartOnButton()}
+                onMouseLeave={()=>this.toggleDisplayHeartOnButton()}>
+                <a href="/newGiveAway">
+                    {this.newFormButtonLabel()}
+                </a>
+            </div>
+        )
+    }
     render() {
         return(
             <div className="navBar">
                 <StyleMenu />
-                <div className="addNewFormButton">
-                    <a href="/newGiveAway">+GiveAway</a>
-                </div>
+                {this.renderNewFormButton()}
             </div>
         )
     }
