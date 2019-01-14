ccc6a9ed18348f25bb596ab8987e213955bf58fa
Hera <jyh106@berkeley.edu>
2019-01-14 12:45:26 -0800

    redesigned navbar

--- src/Components/Header/SearchBox/SearchBox.js
+++ src/Components/Header/SearchBox/SearchBox.js
@@ -4,7 +4,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import React, { Component } from 'react';
 import "./SearchBox.css";
-import Actions from '../../../Actions/actions.js';
 import OutsideAlert from '../../../OutsideClick';
 library.add(faSearch);
 
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import "./NavBar.css";
-import StyleMenu from './StyleMenu/StyleMenu.js';
 import { connect } from 'react-redux';
 import Actions from '../../Actions/actions.js';
+import Utils from "../../utils.js";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-library.add(faHeart);
+import { faHeart, faTh, faThList, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+library.add(faHeart, faTh, faThList, faMapMarkedAlt);
 
 class NavBar extends Component {
     constructor() {
@@ -24,33 +24,61 @@ class NavBar extends Component {
 
     newFormButtonLabel() {
         if (!this.state.displayHeartOnButton) {
-            return "Give"
+            return null
         } 
-        return <FontAwesomeIcon icon="heart"></FontAwesomeIcon>
+        return <FontAwesomeIcon icon="heart" className="icon_heart fadeInHeart"></FontAwesomeIcon>
     }
+
     renderNewFormButton() {
         return (
             <div className="addNewFormButton"
                 onMouseEnter={()=>this.toggleDisplayHeartOnButton()}
                 onMouseLeave={()=>this.toggleDisplayHeartOnButton()}>
                 <a href="/newGiveAway">
-                    {this.newFormButtonLabel()}
+                    Give {this.newFormButtonLabel()}
                 </a>
+            </div>
+        )
+    }
+
+    getActiveClassName(style){
+        if (this.props.currentStyle === style) {
+            return "activeStyle"
+        }
+        return ""
+    }
+
+    renderStyleButtons() {
+        return (
+            <div className="styleBar">
+                <div className={`display_map styleIcon ${this.getActiveClassName('Map')}`}
+                    onClick = {()=>{this.props.changeDisplayStyle('Map')}}>
+                    <FontAwesomeIcon icon="map-marked-alt" className="icon_map"></FontAwesomeIcon>
+                </div>
+                <div className= {`display_list styleIcon ${this.getActiveClassName('List')}`}
+                    onClick = {()=>{this.props.changeDisplayStyle('List')}}>
+                    <FontAwesomeIcon icon="th-list" className="icon_list"></FontAwesomeIcon>
+                </div>
+                <div className={`display_gallery styleIcon ${this.getActiveClassName('Gallery')}`}
+                    onClick = {()=>{this.props.changeDisplayStyle('Gallery')}}>
+                    <FontAwesomeIcon icon="th" className="icon_gallery"></FontAwesomeIcon>
+                </div>
             </div>
         )
     }
     render() {
         return(
             <div className="navBar">
-                <StyleMenu />
                 {this.renderNewFormButton()}
+                {this.renderStyleButtons()}
             </div>
         )
     }
 }
 
 function mapStateToProps(state) {
-    return {
+    return{
+        currentStyle: Utils.getDisplayStyle(state)
     }
   }
 
@@ -58,7 +86,8 @@ const mapDispatchToProps = dispatch => {
     return {
         toggleModal: (type, toggle) => {
             dispatch(Actions.toggleModal(type, toggle));
-        }
+        },
+        changeDisplayStyle: (style)=> dispatch(Actions.changeDisplayStyle(style))
     }
   }
   
