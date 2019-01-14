5b2b2d00bb1f1855acf2ebf530ea1c5e9ee1549b
Hera <jyh106@berkeley.edu>
2019-01-13 17:51:34 -0800

    added account menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,13 +2,75 @@ import React, { Component } from 'react';
 import "./Header.css";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 import { connect } from 'react-redux';
 import Actions from "../../Actions/actions";
 import SearchBox from './SearchBox/SearchBox.js';
-library.add(faAngleDown, faUserCircle) 
+library.add(faUserCircle) 
 
 class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            accountDropDownMenuShown: false,
+            isUserSignedIn: true,
+            username: "Hera",
+        }
+    }
+
+    renderSignedInAccountMenu() {
+        return (
+            <div className="accountDropDownMenu signedInMenu">
+                <div className="account_header">
+                    {this.state.username}
+                </div>
+                <div className="account_profile accountMenuItem">
+                    Profile
+                </div>
+                <div className="account_settings accountMenuItem">
+                    Settings
+                </div>
+                <div className="account_signOut accountMenuItem">
+                    Sign out
+                </div>
+            </div>
+        )
+    }
+
+    renderNotSignedInAccountMenu() {
+        return (
+            <div className="accountDropDownMenu notSignedInMenu">
+                <div className="account_header">
+                    Welcome!
+                </div>
+                <div className="account_logIn accountMenuItem">
+                    Log in
+                </div>
+                <div className="account_signUp accountMenuItem">
+                    Don't have an account
+                </div>
+            </div>
+        )
+    }
+
+    renderAccountDropdownMenu() {
+        if (!this.state.accountDropDownMenuShown) {
+            return null
+        } else {
+            if (!this.state.isUserSignedIn) {
+                return this.renderNotSignedInAccountMenu();
+            } else {
+                return this.renderSignedInAccountMenu();
+            }
+        }
+    }
+
+    toggleAccountMenu() {
+        this.setState({
+            accountDropDownMenuShown: !this.state.accountDropDownMenuShown,
+        })
+    }
+
     render() {
         return(
             <div className="header">
@@ -17,11 +79,11 @@ class Header extends Component {
                 </div>
                 <div className="user">
                     <SearchBox />
-                    <FontAwesomeIcon icon="user-circle" className="icon_user" /> 
-                    <div className="accountDropDownMenu">
-                        <FontAwesomeIcon icon="angle-down" className="icon_angleDown" />
-                    </div>
+                    <FontAwesomeIcon icon="user-circle" 
+                                    className="icon_user" 
+                                    onClick={ ()=> this.toggleAccountMenu()} /> 
                 </div>
+                {this.renderAccountDropdownMenu()}
             </div>
         )
     }
